012bc8c9fa19ba61b699948a0b0f638657c0f5b0
Reuben <reuben@koppenheffer.me>
2019-06-29 06:25:20 -0500

    Implement is_valid_name method

--- array_method.rb
+++ array_method.rb
@@ -65,3 +65,19 @@ end
 
 puts format_name("chase WILSON") # => "Chase Wilson"
 puts format_name("brian CrAwFoRd scoTT") # => "Brian Crawford Scott"
+
+# A name is valid is if satisfies all of the following:
+# - contains at least a first name and last name, separated by spaces
+# - each part of the name should be capitalized
+#
+# Hint: use str.upcase or str.downcase
+# "a".upcase # => "A"
+
+def is_valid_name(str)
+  return str.include?(" ") && format_name(str) == str
+end
+
+puts is_valid_name("Kush Patel")       # => true
+puts is_valid_name("Daniel")           # => false
+puts is_valid_name("Robert Downey Jr") # => true
+puts is_valid_name("ROBERT DOWNEY JR") # => false
