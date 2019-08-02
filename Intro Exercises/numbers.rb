91a360155d8127fe6dce0a29dcfe441c959a3940
Reuben John <rjkoppy@gmail.com>
2019-08-02 05:30:41 -0500

    Add completed exercises

---
+++ Intro Exercises/numbers.rb
@@ -0,0 +1,20 @@
+# Example:
+puts 4 + 2.5   # 6.5
+
+# Write 4 more expressions below:
+puts 1 - 2 # -1
+puts 34 / 12 # 2
+puts 23 % 4 # 3
+puts 17 + 21 # 38
+
+# Example:
+puts 4 == 4       # true
+
+# Write 6 more expressions below:
+
+puts 1 > -23
+puts 21 != 3
+puts 12 < 213
+puts 20 <= 20
+puts 1 == 1
+puts 32 >= 231
