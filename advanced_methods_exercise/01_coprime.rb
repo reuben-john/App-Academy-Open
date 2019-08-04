1623f5c14a4122270a583883c0e4507161051589
Reuben John <rjkoppy@gmail.com>
2019-08-04 06:50:58 -0500

    Solve exercise

--- advanced_methods_exercise/01_coprime.rb
+++ advanced_methods_exercise/01_coprime.rb
@@ -3,6 +3,12 @@
 # The method should return false otherwise. For example coprime?(25, 12) is true because
 # 1 is the only number that divides both 25 and 12.
 
+def coprime?(num_1, num_2)
+  if num_1.gcd(num_2) > 1
+    return false
+  end
+  return true
+end
 
 p coprime?(25, 12)    # => true
 p coprime?(7, 11)     # => true
